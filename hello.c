bd348070ab7751f38ca5334575788eb92df7da48
Sixels <sixels@protonmail.com>
2021-11-18 19:40:16 -0300

    Hello World without Hello World

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void) {
+  unsigned long al = 0x726F6C6564574820,
+                dx = 0x357620655410;
+  while(dx) putchar(al >> (((dx>>=4) & 0xF) << 3) & 0xFF);
+}
---
+++ magic.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+
+void print_bin(uint64_t);
+#define uint unsigned int
+
+static char message[] = "Hello World";
+
+int main(void) {
+
+  // Get the individual alphabet of the message
+  char alphabet[sizeof(message)] = {};
+  {
+    // find the number occurrences of each character in the message
+    char ascii[256] = {};
+    for (uint j = 0; j < sizeof(message)-1; ++j) {
+      ascii[(int)message[j]]+=1;
+    }
+    int idx = 0;
+    for (uint i = 0; i < sizeof(ascii); ++i) {
+      if (ascii[i] > 0) {
+        alphabet[idx++] = i;
+      }
+    }
+    // this asserts alphabet will be null terminated
+    alphabet[idx] = 0;
+  }
+
+  // create a table of indices of each letter
+  int indx[sizeof(message)-1] = {};
+  {
+      for (uint i = 0; i < sizeof(message)-1; ++i) {
+        char c = message[i];
+        for (uint j = 0; alphabet[j]; ++j) {
+          if (alphabet[j] == c) {
+            indx[i] = j;
+          }
+        }
+      }
+  }
+
+  // encode the indices
+  uint64_t encoded_indx = 0;
+  // each indice is a 4bit number, so we can put two indices in a single byte
+  // this will result in a len(indx)/2 bytes long number.
+  for (uint i = 0; i < sizeof(message) - 1; ++i) {
+    encoded_indx |= (uint64_t)indx[i] << 4*i;
+  }
+  encoded_indx <<= 4;
+
+  // encode the alphabet
+  unsigned long encoded_alphabet = 0;
+  // the alphabet is 8 characters long
+  // each character is 7 bits long
+  for (uint i = 0; i < sizeof(alphabet)-1; ++i) {
+    encoded_alphabet |= (uint64_t)alphabet[i] << 8*i;
+    /* print_bin(encoded_alphabet); */
+    /* puts(""); */
+  }
+
+  printf("alphabet: \"%s\"\n", alphabet);
+  printf("indx: {");
+  for (uint i = 0; i < sizeof(message)-1; ++i) {
+    printf("%d,", indx[i]);
+  }
+  printf("}\n");
+  printf("encoded_indx: 0x%"PRIX64"\n", encoded_indx);
+  printf("encoded_alphabet: 0x%"PRIX64"\n", encoded_alphabet);
+
+  return 0;
+}
+
+#include <limits.h>
+
+void print_bin(uint64_t integer)
+{
+    int i = CHAR_BIT * sizeof integer; /* however many bits are in an integer */
+    while(i--) {
+        putchar('0' + ((integer >> i) & 1));
+    }
+}
